e51dbfce4f93170fd9545cc6c8c0a79b4bd6c79b
unknown <victor.ibarra.olivares@gmail.com>
2021-03-23 12:40:19 -0300

    se mejora funcionalidad del envio del email al estar escribiendolo, solo se envia al momento de apretar el boton. Se muestra animación al suscriptor al momento de esperar la suscripción

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -48,13 +48,14 @@
                 type="email"
                 name="email"
                 placeholder="Email address"
-                wire:model="email"
+                wire:model.defer="email"
             ></x-input>
             <span class="text-gray-100 text-xs" >
                 {{ $errors->has('email') ? $errors->first('email') : 'We will send you a confirmation email.' }}
             </span>
             <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center " >
-                Get In
+                <span class="animate-spin" wire:loading wire:target="subscribe" >&#9696;</span>
+                <span wire:loading.remove wire:target='subscribe' >Get In</span>
             </x-button>
         </form>
     </x-modal>
