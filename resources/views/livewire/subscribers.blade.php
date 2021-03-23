58529fdea431e05c0ccf4f9ffcb6d16d1d882a16
unknown <victor.ibarra.olivares@gmail.com>
2021-03-23 15:57:27 -0300

    se permite eliminar suscriptor de la lista utilizando livewire

---
+++ resources/views/livewire/subscribers.blade.php
@@ -0,0 +1,39 @@
+<div class="p-6 bg-white border-b border-gray-200">
+    <p class="text-2xl text-gray-600 font-bold mb-6 underline">
+        Subscribers
+    </p>
+
+    @if ( $subscribers->isEmpty() )
+        <div class="flex w-full bg-red-200 p-5 rounded-lg">
+            <p class="text-red-700" >No subscribers found.</p>
+        </div>
+    @else
+        <table class="w-full">
+            <thead 
+                class="border-b-2 border-gray-300 text-indigo-600 " 
+                >
+                <tr>
+                    <th class="px-6 py-3 text-left" >Email</th>
+                    <th class="px-6 py-3 text-left" >Verified</th>
+                    <th class="px-6 py-3 text-left">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($subscribers as $subscriber)
+                    <tr class="text-sm text-indigo-900 border-b border-gray-400" >
+                        <td class="px-6 py-4" >{{ $subscriber->email }}</td>
+                        <td class="px-6 py-4" >{{ optional($subscriber->email_verified_at)->diffForHumans() ?? 'Never' }}</td>
+                        <td class="px-6 py-4" >
+                            <x-button 
+                                class="border border-red-500 text-red-500 hover:bg-red-100 bg-red-50" 
+                                wire:click='delete({{ $subscriber->id }})'
+                                >
+                                Delete
+                            </x-button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+</div>
